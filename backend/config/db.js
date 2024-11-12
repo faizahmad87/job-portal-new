ed1bfde2466dcf2f12ce801be552de7c2117a255
Faiz Ahmad <97790544+faizahmad87@users.noreply.github.com>
2024-11-12 10:42:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const mongoURI = process.env.MONGODB_URI;
 console.log(mongoURI);
 const connectDB = async () => {
  try {
-  await mongoose.connect(mongoURI);
+  await mongoose.connect("mongodb+srv://faizahmad9608013206:G8noVovIRMJOjktt@job-portal.y7q25.mongodb.net/test");
   console.log('MongoDB Connected');
  } catch (err) {
   console.error(err.message);
