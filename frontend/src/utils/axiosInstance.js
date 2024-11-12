a3958259d0e1859ac1ab59806451041dbc4edead
Faiz Ahmad <97790544+faizahmad87@users.noreply.github.com>
2024-11-12 09:20:19 +0530

    added new server link

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL
 const axiosInstance = axios.create({
- baseURL: 'https://job-portal-rho-nine.vercel.app/api', // This will be the base URL for all requests
+ baseURL: 'https://job-portal-back-blush.vercel.app/api', // This will be the base URL for all requests
  timeout: 50000 // Optional: set a timeout for requests
 });
 
