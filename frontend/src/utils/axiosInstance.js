a25020ce39bd8ed9bcef8ec783ac48c3f73a99eb
Faiz Ahmad <97790544+faizahmad87@users.noreply.github.com>
2024-11-12 12:18:39 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL
 const axiosInstance = axios.create({
- baseURL: 'https://job-portal-rho-nine.vercel.app/api', // This will be the base URL for all requests
+ baseURL: 'https://job-portal-new-4.onrender.com/api', // This will be the base URL for all requests
  timeout: 500000 // Optional: set a timeout for requests
 });
 
