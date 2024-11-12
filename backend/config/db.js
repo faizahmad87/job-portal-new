d5e88515145dd3ba891c562ba057a73d21f75cf0
Faiz Ahmad <97790544+faizahmad87@users.noreply.github.com>
2024-11-12 10:34:38 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 // config/db.js
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI;
+
+console.log(mongoURI);
 const connectDB = async () => {
  try {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(mongoURI);
   console.log('MongoDB Connected');
  } catch (err) {
   console.error(err.message);
