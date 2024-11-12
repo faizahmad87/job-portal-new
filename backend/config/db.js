9f6fc162973c0b9965761a8d539feeea92a13e7e
Faiz Ahmad <97790544+faizahmad87@users.noreply.github.com>
2024-11-12 11:56:55 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
  try {
   await mongoose.connect('mongodb://faizahmad9608013206:G8noVovIRMJOjktt@job-portal.y7q25.mongodb.net/test?retryWrites=true&w=majority',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
   socketTimeoutMS: 45000 // Adjust socket timeout
 });
